0c59c6328d7b36c4edb27719ef8e322977043dab
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-03-16 10:23:25 -0600

    seeded the departments table

--- index.js
+++ index.js
@@ -13,18 +13,38 @@ const db = mysql.createConnection(
       host: 'localhost',
       // MySQL username,
       user: 'root',
-      // MySQL password
-      password: '',
-      database: 'courses_db'
+      // MySQL password created with a random password genorator
+      password: 'S$U%Ry2AT%9To8A29hro6h4cWr',
+
+
+    //   may change database name
+      database: 'hr_db'
     },
     console.log(`Connected to the courses_db database.`)
   );
   
   // Hardcoded query: DELETE FROM course_names WHERE id = 3;
-  
-  db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
+
+
+
+  db.query('SELECT * FROM departments', function (err, results) {
+    console.log(results);
   });
+
+
+//   db.query('SELECT * FROM roles', function (err, results) {
+//     console.log(results);
+//   });
+
+//   db.query('SELECT * FROM employees', function (err, results) {
+//     console.log(results);
+//   });
+
+
+  
+//   db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
